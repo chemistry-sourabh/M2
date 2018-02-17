dd29bbbb56ccddcb6387912caece9d6a0e5c5c91
Sourabh Bollapragada <chemistry_sourabh@yahoo.co.in>
2018-02-18 03:27:12 +0530

    Added meeting updates

--- m2/core.py
+++ m2/core.py
@@ -13,6 +13,7 @@ class Core:
     # Can be extended for DB Auth by doing some architecture change
     # Authentication Driver should be used in rest.py to get entity object
     # and passed here.
+    # TODO Should check if entity is in quota table before proceeding
     def __init__(self, entity, config, driver):
         """
         Initializes DB and other parameters
@@ -29,9 +30,14 @@ class Core:
         self.db = None
 
         self.driver = driver
+        self.config = config
         self.entity_id = entity.id
         self.entity_is_admin = entity.is_admin
-        self.config = config
+
+        try:
+            self.db.quota.get_info(self.entity_id)
+        except Exception:
+            self.db.quota.insert(self.entity_id, self.config.quota_default)
 
     # provision_engine_id is in rest_api.md, but do we really need it ?
     # Not sure where the extra_params are required, we can leave it as it is
@@ -80,38 +86,16 @@ class Core:
 
             # Create a clone (copy on write) of the parent image and associate
             # with the instance
-            clone_id = self.driver.storage.clone(instance_id, parent_image_id)
+            self.driver.storage.clone(instance_id, parent_image_id)
 
-            # Should mount the clone on the diskless server and create a
-            # target, there are 2 problems here:-
+            # Get Clone Name and Other information needed for diskless
+            clone_info = self.driver.storage.get_clone_info(instance_id)
 
-            # 1. Some operations may need to be done to setup clone like for
-            # example a rbd map needs to be done for IET.
+            # Add Target To Diskless
+            self.driver.diskless.add_target(instance_id, clone_info)
 
-            # 2. The clone information needed depends on diskless like for
-            # example, TGT needs clone name whereas IET needs mapped rbd path.
-
-            # To solve this
-            # * Each diskless driver should provide another interface called
-            # Diskless Storage interface that either the storage driver or some
-            # other class can implement.
-            # * There should a Diskless Storage driver for every
-            # diskless-storage combination we want to support.
-            # * The diskless storage driver is going to be passed as an
-            # argument to diskless driver during init and the diskless driver
-            # is going to use it in mount and unmount clone calls.
-            target_id = self.driver.diskless.add_target(instance_id, clone_id)
-
-            # I dont see a reason yet to write a pxe driver as PXE, DHCP and
-            # TFTP are all protocols and driver is just going to generate files
-            # that follow a template. We can hardcode a driver in the driver
-            # loading phase and pass a key (self.config.diskless.key) that will
-            # tell the driver which template to use as the template is diskless
-            # dependent. Incase we need a driver in the future should be less
-            # than 10 lines.
             # Generates the mac address and ipxe files.
-            self.driver.pxe.register(instance_id, mac_address, target_id, nic,
-                                     self.config.diskless.key)
+            self.driver.diskless.register(instance_id, mac_address, nic)
 
             return instance_id
 
@@ -138,13 +122,11 @@ class Core:
                         != self.entity_id:
                     raise Exception
 
-            # Get Target Id associated with instance id
-            target_id = self.driver.diskless.get_target_id(instance_id)
             # Delete Mac Address and IPXE files associated with id
-            self.driver.pxe.unregister(instance_id)
+            self.driver.diskless.unregister(instance_id)
             # Register target with new node
-            self.driver.pxe.register(instance_id, dest_mac_address, target_id,
-                                     dest_nic, self.config.diskless.key)
+            self.driver.diskless.register(instance_id, dest_mac_address,
+                                          dest_nic)
 
         except Exception:
             pass
@@ -164,7 +146,7 @@ class Core:
                 if instance.entityId != self.entity_id:
                     raise Exception
 
-            self.driver.pxe.unregister(instance_id)
+            self.driver.diskless.unregister(instance_id)
             # Unmount Clone from diskless
             self.driver.diskless.delete_target(instance_id)
             # Delete Clone in Storage
@@ -194,23 +176,13 @@ class Core:
                         != self.entity_id:
                     raise Exception
 
-            # This is how it is currently done, but shouldnt be as we are
-            # dictating that the clone should have a parent_image_id in the
-            # driver
-            # clone = self.driver.storage.get_clone_info(instance_id)
-            # image = self.driver.storage.get_image_info(clone.parent_image_id)
-            # parent_image_id = image.image_id
-
-            # This is not possible currently as provisioned instance table
-            # doesnt have a foreign key to image table, but I feel this is the
-            # right way.
             parent_image_id = self.db.provisionedInstance. \
                 get_info(instance_id).parent_image_id
 
             # Insert Snapshot into DB
-            global_image = self.db.image.get_info(parent_image_id)
+            parent_image = self.db.image.get_info(parent_image_id)
             snap_image_id = self.db.image.insert(snap_name, self.entity_id,
-                                                 global_image.type,
+                                                 parent_image.type,
                                                  isSnapshot=True)
 
             # Take Snapshot
@@ -221,7 +193,6 @@ class Core:
         except Exception:
             pass
 
-    # Add restore_tag to the rest api?
     def tag(self, instance_id, tag_name):
         """
         Create a shallow copy of the provisioned instance's current state
@@ -238,10 +209,8 @@ class Core:
                         != self.entity_id:
                     raise Exception
 
-            # Turns is_active bit for all tags under instance_id to False
-            self.db.tag.deactivate_all_tags(instance_id)
             # Insert new Tag
-            tag_id = self.db.tag.insert(tag_name, instance_id, is_active=True)
+            tag_id = self.db.tag.insert(tag_name, instance_id)
 
             # Create Tag
             self.driver.storage.tag(instance_id, tag_id)
@@ -251,13 +220,10 @@ class Core:
         except Exception:
             pass
 
-    # Confusion about Tag commands regarding Ids. Need to discuss
-    # Assuming that Id is going to be unique under a provisioned instance id
-    def update_tag(self, instance_id, tag_id, info):
+    def update_tag(self, tag_id, info):
         """
         Update Tag Meta Data
 
-        :param instance_id: Id of the provisioned instance
         :param tag_id: Id of the tag
         :param info: Dictionary of new info that needs to be updated
         :return: None
@@ -266,24 +232,23 @@ class Core:
         try:
 
             if not self.entity_is_admin:
-                if self.db.provisionedInstance.get_info(instance_id).entityId \
+
+                instance_id = self.db.tag.get_info(tag_id).instance_id
+
+                if self.db.provisionedInstance.get_info(instance_id).entityd \
                         != self.entity_id:
                     raise Exception
 
-            # Check If Tag Exists
-            self.db.tag.get_info(instance_id, tag_id)
-
             # Update Tag
-            self.db.tag.update(instance_id, tag_id, info)
+            self.db.tag.update(tag_id, info)
 
         except Exception:
             pass
 
-    def delete_tag(self, instance_id, tag_id):
+    def delete_tag(self, tag_id):
         """
         Delete Tag
 
-        :param instance_id: Id of the provisioned instance
         :param tag_id: Id of the tag
         :return: None
         """
@@ -291,16 +256,15 @@ class Core:
         try:
 
             if not self.entity_is_admin:
-                if self.db.provisionedInstance.get_info(instance_id).entityId \
+                instance_id = self.db.tag.get_info(tag_id).instance_id
+
+                if self.db.provisionedInstance.get_info(instance_id).entityd \
                         != self.entity_id:
                     raise Exception
 
-            # Check If Tag Exists
-            self.db.tag.get_info(instance_id, tag_id)
-
             # Delete Tag in Storage and in db.
-            self.driver.storage.delete_tag(instance_id, tag_id)
-            self.db.tag.delete(instance_id, tag_id)
+            self.driver.storage.delete_tag(tag_id)
+            self.db.tag.delete(tag_id)
 
         except Exception:
             pass
@@ -326,33 +290,34 @@ class Core:
         except Exception:
             pass
 
-    def show_tag(self, instance_id, tag_id):
+    def show_tag(self, tag_id):
         """
         Get Tag Meta Data
 
-        :param instance_id: Id of the provisioned instance
         :param tag_id: Id of the tag
         :return: Tag Info as an object
         """
 
         try:
 
+            # Get Tag Info
+            tag_info = self.db.tag.get_info(tag_id)
+
             if not self.entity_is_admin:
-                if self.db.provisionedInstance.get_info(instance_id).entityId \
-                        != self.entity_id:
+
+                if self.db.provisionedInstance.get_info(tag_info.instance_id) \
+                        .entity_id != self.entity_id:
                     raise Exception
 
-            # Get and return info
-            return self.db.tag.get_info(instance_id, tag_id)
+            return tag_info
 
         except Exception:
             pass
 
-    def flatten_tag(self, instance_id, tag_id, image_name):
+    def flatten_tag(self, tag_id, image_name):
         """
         Flatten Tag into an image
 
-        :param instance_id: Id of the provisioned instance
         :param tag_id: Id of the tag
         :param image_name: name of the flattened image
         :return: Image Id
@@ -360,14 +325,15 @@ class Core:
 
         try:
 
+            instance_id = self.db.tag.get_info(tag_id).instance_id
+
             if not self.entity_is_admin:
-                if self.db.provisionedInstance.get_info(instance_id).entityId \
+
+                if self.db.provisionedInstance.get_info(instance_id).entityd \
                         != self.entity_id:
                     raise Exception
 
-            self.db.tag.get_info(instance_id, tag_id)
-
-            # Get Parent Image, Check comments in snapshot
+            # Get Parent Image
             parent_image_id = self.db.provisionedInstance. \
                 get_info(instance_id).parent_image_id
             parent_image = self.db.image.get_info(parent_image_id)
@@ -377,7 +343,7 @@ class Core:
                                             parent_image.type)
 
             # Flatten Tag
-            self.driver.storage.flatten_tag(instance_id, tag_id, image_id)
+            self.driver.storage.flatten_tag(tag_id, image_id)
 
             return image_id
 
@@ -420,9 +386,6 @@ class Core:
         except Exception:
             pass
 
-    # Function should be added for inserting new entity into quota table.
-    # Should check if entity is in quota table before proceeding
-    # Add current_usage column to Quota to avoid long counting operations ?
     def update_quota(self, entity_id, info):
         """
         Update Entity's quota object with new info
@@ -508,6 +471,10 @@ class Core:
     # Didnt figure out from last time how to implement this.
     # Do we want to support multiple datastores right now. For the first
     # prototype version we can implement a single datastore version
+    # Ignoring Datastore Id for now as There is no datastore_id in schema,
+    # Can use it when schema is updated
+    # Get Supported Types can be implemented in rest as it is a constansts list
+    # Get Supported Types is needed for upload.
     def upload(self, image_name, image_type, datastore_id=None,
                is_public=False,
                url=None):
@@ -648,6 +615,3 @@ class Core:
 
         except Exception:
             pass
-
-    # Get Supported Types can be implemented in rest as it is a constansts list
-    # How to implement List Datastores and List Provisioning Engines
